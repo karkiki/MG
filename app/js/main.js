8fbd6a6b52170f794bbee245c5a0782ba76fa1fb
karkiki <larinaksenia9@icloud.com>
2023-05-25 22:47:34 +0300

    finish header

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#search").on('click', function () {
+        $(".menu-item").addClass('hide-item');
+        $(".header__search-form").addClass('active');
+        $(".close").addClass('active');
+        $("#search").hide();
+    })
+    $(".close").on('click', function () {
+        $(".menu-item").removeClass('hide-item');
+        $(".header__search-form").removeClass('active');
+        $(".close").removeClass('active');
+        $("#search").show();
+    })
+
+    //Sticky scroll header
+    window.addEventListener("scroll", function() {
+        const header = this.document.querySelector(".header__top-inner");
+        header.classList.toggle("sticky", this.window.scrollY > 0)
+    })
+});
